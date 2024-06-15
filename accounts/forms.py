d66d16d23b7702679f709cdc71613ddd20cb1472
Kirill777-web <ki.lazutin@gmail.com>
2024-06-15 13:37:29 -0300

    changes in form

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,10 +3,6 @@ from django.contrib.auth.models import User
 from .models import UserProfile
 
 
-from django import forms
-from django.contrib.auth.models import User
-
-
 class UserRegistrationForm(forms.ModelForm):
     password1 = forms.CharField(
         label='Password', widget=forms.PasswordInput(attrs={'class': 'form-control'}))
@@ -18,6 +14,22 @@ class UserRegistrationForm(forms.ModelForm):
         attrs={'class': 'form-control'}))
     income = forms.DecimalField(max_digits=10, decimal_places=2, widget=forms.NumberInput(
         attrs={'class': 'form-control'}))
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+            self.save_profile(user)
+        return user
+
+    def save_profile(self, user):
+        UserProfile.objects.create(
+            user=user,
+            profession=self.cleaned_data["profession"],
+            savings=self.cleaned_data["savings"],
+            income=self.cleaned_data["income"],
+        )
 
     class Meta:
         model = User
